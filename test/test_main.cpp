6ccc365f19ed2913d27921257a103cb3455380d6
Karol <Szlagowski.k.j@gmail.com>
2024-04-19 23:51:30 +0200

    Azimuth test

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,13 +1,16 @@
 #include <unity.h>
+#include <config.h>
 
 float standarize_deg(int32_t lon_or_lat);
 void standarize_deg_test();
 float calculate_azimuth(int32_t tracker_x, int32_t tracker_y, int32_t object_x, int32_t object_y);
+void calculate_azimuth_test();
 
 int main(int argc, char **argv)
 {
     UNITY_BEGIN();
     RUN_TEST(standarize_deg_test);
+    RUN_TEST(calculate_azimuth_test);
     UNITY_END();
     return 0;
 }
@@ -22,9 +25,25 @@ void standarize_deg_test()
     int32_t lon_or_lat = 61.256 * pow(10, 7);
     float expected = 61.256;
     float acutal = standarize_deg(lon_or_lat);
-    TEST_ASSERT_EQUAL_DOUBLE(expected, acutal);
+    TEST_ASSERT_EQUAL_FLOAT(expected, acutal);
 }
 
 float calculate_azimuth(int32_t tracker_x, int32_t tracker_y, int32_t object_x, int32_t object_y)
 {
+    object_x = object_x - tracker_x;
+    object_y = object_y - tracker_y;
+    float standarized_object_x = standarize_deg(object_x);
+    float standarized_object_y = standarize_deg(object_y);
+    return ((float)(atan2(object_x, object_y)) * (180 / PI));
+}
+
+void calculate_azimuth_test()
+{
+    int32_t tracker_x = 1 * pow(10, 7);
+    int32_t tracker_y = 1 * pow(10, 7);
+    int32_t object_x = 2 * pow(10, 7);
+    int32_t object_y = 2 * pow(10, 7);
+    float expected = 45.02282;
+    float acutal = calculate_azimuth(tracker_x, tracker_y, object_x, object_y);
+    TEST_ASSERT_EQUAL_FLOAT(expected, acutal);
 }
