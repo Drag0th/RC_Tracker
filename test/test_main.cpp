87b61f6439b1c31ef0713941035df540a2acba75
Karol <Szlagowski.k.j@gmail.com>
2024-04-20 23:54:25 +0200

    Azimuth optimization

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -5,12 +5,15 @@ float standarize_deg(int32_t lon_or_lat);
 void standarize_deg_test();
 float calculate_azimuth(int32_t tracker_x, int32_t tracker_y, int32_t object_x, int32_t object_y);
 void calculate_azimuth_test();
+float optimize_azimuth(float current_deg, float destination_deg);
+void optimze_azimuth_test();
 
 int main(int argc, char **argv)
 {
     UNITY_BEGIN();
     RUN_TEST(standarize_deg_test);
     RUN_TEST(calculate_azimuth_test);
+    RUN_TEST(standarize_deg_test);
     UNITY_END();
     return 0;
 }
@@ -45,5 +48,42 @@ void calculate_azimuth_test()
     int32_t object_y = 2 * pow(10, 7);
     float expected = 45.02282;
     float acutal = calculate_azimuth(tracker_x, tracker_y, object_x, object_y);
+    TEST_ASSERT_EQUAL_FLOAT(expected, acutal);
+}
+
+float optimize_azimuth(float current_deg, float destination_deg)
+{
+    float A, B, C;
+    A = destination_deg - current_deg;
+    B = destination_deg - current_deg - 360;
+    B = destination_deg - current_deg + 360;
+    if (abs(A) < abs(B))
+    {
+        if (abs(A) < abs(C))
+        {
+            return A;
+        }
+        else
+        {
+            return C;
+        }
+    }
+    else
+    {
+        if (abs(B) < abs(C))
+        {
+            return B;
+        }
+        else
+        {
+            return C;
+        }
+    }
+}
+
+void optimze_azimuth_test()
+{
+    float expected = -32.4;
+    float acutal = optimize_azimuth(20, 352.4);
     TEST_ASSERT_EQUAL_FLOAT(expected, acutal);
 }
